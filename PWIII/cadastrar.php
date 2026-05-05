b6c8981ccf0c4b71edc80b70b4c40e1ffc8fd3f2
Maria Alves <mariaclaraalvesoliveira53@gmail.com>
2026-05-05 10:36:23 -0300

    Delete PWIII/cadastrar.php

--- PWIII/cadastrar.php
+++
@@ -1,8 +0,0 @@
- CREATE DATABASE etimusuario;
- use etimusuario;
-CREATE TABLE usuario(
-    id int primary key auto_increment,
-    nome varchar(200) not null,
-    email varchar(200) not null unique,
-    senha varchar(200) not null
-)
