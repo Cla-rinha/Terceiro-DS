0bf60995026e7d6c6a3affb2c7492c054f9733e3
Maria Alves <mariaclaraalvesoliveira53@gmail.com>
2026-08-25 10:52:24 -0300

    Add files via upload
    Tela de cadastrar produtos: formulário

---
+++ PWIII/projetoPW3_18-08/sql/banco.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE IF NOT EXISTS loja_db;
+USE loja_db;
+
+CREATE TABLE IF NOT EXISTS produtos (
+    id_produto int AUTO_INCREMENT PRIMARY KEY,
+    nome_produto varchar(100),
+    descricao text,
+    valor double
+);
+
+CREATE TABLE IF NOT EXISTS imagens (
+    id_imagem int AUTO_INCREMENT PRIMARY KEY,
+    nome_imagem varchar(100),
+    fk_id_produto int,
+    FOREIGN KEY(fk_id_produto)REFERENCES produtos(id_produto)
+);
